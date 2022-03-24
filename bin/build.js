39ac24354f146b878722a764917180d91ab2e54d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-25 00:54:57 +0100

    Clean up

--- bin/build.js
+++ bin/build.js
@@ -1,23 +1,23 @@
-import yaml from 'js-yaml';
-import fs from 'fs';
-import { getDirname, getFiles, getIdFromFilename, getNrFromId } from './utils.js';
+import {
+    getDirname,
+    getFiles,
+    getIdFromFilename,
+    getNrFromId,
+    readYaml,
+    writeJson,
+ } from './utils.js';
 
 const __dirname = getDirname(import.meta.url);
 
-const inputDir = `${__dirname}/../meta/`;
-
-const files = getFiles(inputDir);
+const files = getFiles(`${__dirname}/../meta/`);
 
 const data = [];
 
 files.forEach((file) => {
-    const fileContent = yaml.load(fs.readFileSync(file, 'utf8'));
+    const fileContent = readYaml(file);
     fileContent.id = getIdFromFilename(file, 'utf8');
     fileContent.nr = getNrFromId(fileContent.id);
     data.push(fileContent);
 });
 
-const outputDir = `${__dirname}/../`;
-
-fs.mkdir(outputDir, { recursive: true }, () => {});
-fs.writeFileSync(`${outputDir}meta.json`, JSON.stringify(data));
+writeJson(`${__dirname}/../meta.json`, data);
