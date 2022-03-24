49add049818c387104d160ac60eddac4da6fa1f0
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-24 02:00:31 +0100

    Filter hidden files

--- bin/utils.js
+++ bin/utils.js
@@ -2,7 +2,8 @@ import fs from 'fs';
 
 export function getFiles(directory, fileList) {
     fileList = fileList || [];
-    const files = fs.readdirSync(directory);
+    let files = fs.readdirSync(directory);
+    files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
     for (let i in files) {
         const name = `${directory}/${files[i]}`;
         if (fs.statSync(name).isDirectory()) {
