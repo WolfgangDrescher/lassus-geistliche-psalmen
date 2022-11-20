5b97b692513d511af1aee346a7500254a6822bfc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-20 13:46:15 +0100

    Sort props in yaml files

--- scripts/utils.js
+++ scripts/utils.js
@@ -54,6 +54,7 @@ export function writeYaml(file, object, options) {
     const data = yaml.dump(object, Object.assign({
         indent: 4,
         lineWidth: -1,
+        sortKeys: true,
     }, options || {}));
     fs.writeFileSync(file, data)
     return yaml.load();
