e59e987483a3b61bfca528b951ea5398e1f9e985
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-20 13:46:01 +0100

    Add prange script

---
+++ scripts/prange.js
@@ -0,0 +1,73 @@
+import { exec } from 'node:child_process';
+
+import {
+    getDirname,
+    getFiles,
+    readYaml,
+    writeYaml,
+ } from './utils.js';
+
+const __dirname = getDirname(import.meta.url);
+
+const files = getFiles(`${__dirname}/../kern`);
+
+function parsePrange(output) {
+    console.log(output);
+    const prange = {
+        noteCount: [],
+        tessitura: null,
+        mean: null,
+        median: null,
+    };
+    const lines = output.split(/\r?\n|\r|\n/g).filter(l => l);
+    lines.forEach(line => {
+        if (!(line.startsWith('*') || line.startsWith('!'))) {
+            // console.log(line, );
+            const [keyno, kern, count] = line.split('\t');
+            prange.noteCount.push({
+                keyno: parseInt(keyno, 10),
+                kern,
+                count: parseInt(count, 10),
+            });
+        }
+        if (line.startsWith('!!')) {
+            const regex = new RegExp(/^!!([a-z]+):\s?([\d.]*)\s?[\w]*(\((.+)\))?$/gm);
+            const [full, prop, num, _, note] = regex.exec(line);
+            if (prop === 'tessitura') {
+                prange[prop] = parseInt(num, 10);
+            } else {
+                prange[prop] = {
+                    keyno: parseFloat(num),
+                    kern: note,
+                };
+            }
+            console.log([prop, num, note]);
+        }
+    });
+    return prange;
+}
+
+files.forEach((file) => {
+    const yamlFile = file.replace('kern', 'meta').replace('krn', 'yaml');
+    const voices = [
+        { spine: 1, name: 'bassus' },
+        { spine: 3, name: 'tenor' },
+        { spine: 5, name: 'cantus' },
+    ];
+    voices.forEach(voice => {
+        exec(`extract -f ${voice.spine} ${file} | prange`, (err, stdout, stderr) => {
+            if (err) return console.error(stderr);
+            const yamlConfig = readYaml(yamlFile);
+            yamlConfig.voices = yamlConfig.voices || {};
+            yamlConfig.voices[voice.name] = yamlConfig.voices[voice.name] || {};
+            yamlConfig.voices[voice.name].prange = parsePrange(stdout);
+            writeYaml(yamlFile, yamlConfig);
+        });
+    });
+    exec(`prange ${file}`, (err, stdout, stderr) => {
+        if (err) return console.error(stderr);
+        const yamlConfig = readYaml(yamlFile);
+        yamlConfig.prange = parsePrange(stdout);
+        writeYaml(yamlFile, yamlConfig);
+    });
+});
