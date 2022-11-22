2023dab3301304c93a624df14c3b261834cb03e2
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-22 15:30:41 +0100

    Use execSync

--- scripts/voice-lyrics.js
+++ scripts/voice-lyrics.js
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
 import {
     getDirname,
     getFiles,
@@ -14,28 +14,25 @@ const __dirname = getDirname(import.meta.url);
 const files = getFiles(`${__dirname}/../kern`);
 
 files.forEach((file) => {
-    exec(`lyrics ${file}`, (err, stdout, stderr) => {
-        if (err) {
-            return;
-        }
-        const piece = {
-            voices: {},
-        };
-        let parts = stdout.split('==');
-        piece.title = parts.shift().split(':')[1].trim();
-        parts = parts.map(line => {
-            return line.trim();
-        });
-        parts = splitInChunks(parts, 2);
-        parts.forEach(([key, lyrics]) => {
-            key = key.toLowerCase();
-            piece.voices[key] = {};
-            piece.voices[key].lyrics = lyrics.split(/(?<=\/)/).map(line => line.trim()).join('\n');
-        });
-        const id = getIdFromFilename(file);
-        const metaFile = `${__dirname}/../meta/${id}.yaml`;
-        const metaData = readYaml(metaFile);
-        mergeDeep(metaData, {voices: piece.voices});
-        writeYaml(metaFile, metaData);
+    const stdout = execSync(`lyrics ${file}`);
+
+    const piece = {
+        voices: {},
+    };
+    let parts = stdout.toString().split('==');
+    piece.title = parts.shift().split(':')[1].trim();
+    parts = parts.map(line => {
+        return line.trim();
     });
+    parts = splitInChunks(parts, 2);
+    parts.forEach(([key, lyrics]) => {
+        key = key.toLowerCase();
+        piece.voices[key] = {};
+        piece.voices[key].lyrics = lyrics.split(/(?<=\/)/).map(line => line.trim()).join('\n');
+    });
+    const id = getIdFromFilename(file);
+    const metaFile = `${__dirname}/../meta/${id}.yaml`;
+    const metaData = readYaml(metaFile);
+    mergeDeep(metaData, {voices: piece.voices});
+    writeYaml(metaFile, metaData);
 });
