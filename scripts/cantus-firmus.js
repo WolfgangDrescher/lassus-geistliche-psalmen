8ef3b29a0a9c920a2d7e12ee7f4c6a0e5395f418
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-12-19 13:39:08 +0100

    Add cantus-firmus.js

---
+++ scripts/cantus-firmus.js
@@ -0,0 +1,55 @@
+import { execSync } from 'child_process';
+import {
+    getDirname,
+    getFiles,
+    readYaml,
+    writeYaml,
+    getIdFromFilename,
+    mergeDeep
+} from './utils.js';
+
+const __dirname = getDirname(import.meta.url);
+
+const files = getFiles(`${__dirname}/../kern`);
+
+const voices = [
+    {
+        name: 'cantus',
+        spine: 5,
+    },
+    {
+        name: 'tenor',
+        spine: 3,
+    },
+    {
+        name: 'bassus',
+        spine: 1,
+    },
+];
+
+files.forEach(file => {
+    const id = getIdFromFilename(file);
+    const metaFile = `${__dirname}/../meta/${id}.yaml`;
+
+    const cantusFirmusInVoices = [];
+
+    voices.forEach(voice => {
+        const voiceData = execSync(`extract -f ${voice.spine} ${file}`).toString();
+        const re = /!LO:TX(.*)t=c\.f\./;
+        if (re.exec(voiceData) !== null) {
+            cantusFirmusInVoices.push(voice.name);
+        }
+    });
+
+    let cantusFirmus = null;
+    if (cantusFirmusInVoices.length === 1) {
+        cantusFirmus = cantusFirmusInVoices[0];
+    }
+
+    const metaConfig = {
+        cantusFirmus,
+    };
+    
+    const currentMetaConfig = readYaml(metaFile);
+    writeYaml(metaFile, mergeDeep(currentMetaConfig, metaConfig));
+});
