7c96b773fe7dc6278ae3e62aced7ee01acf259af
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-02-26 23:40:04 +0100

    Set default yaml config to empty object

--- scripts/generate-yaml.js
+++ scripts/generate-yaml.js
@@ -13,6 +13,6 @@ files.forEach((file) => {
     const id = getIdFromFilename(file);
     const metaFile = `${__dirname}/../meta/${id}.yaml`;
     if (!existsSync(metaFile)) {
-        writeYaml(metaFile);
+        writeYaml(metaFile, {});
     }
 });
