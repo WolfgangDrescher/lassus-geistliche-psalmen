c70a92214991c5e4ffafee3396a3c256ab0b2c7e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-10 20:48:18 +0100

    Add end sound analysis

--- scripts/build.js
+++ scripts/build.js
@@ -2,4 +2,5 @@ import './reference-records.js';
 import './cantus-firmus.js';
 import './voice-lyrics.js';
 import './prange.js';
+import './end-sound.js';
 // import './generate-meta.js';
---
+++ scripts/end-sound.js
@@ -0,0 +1,55 @@
+import { execSync } from 'child_process';
+import {
+    getDirname,
+    getFiles,
+    splitInChunks,
+    readYaml,
+    writeYaml,
+    getIdFromFilename,
+    mergeDeep,
+} from './utils.js';
+
+const __dirname = getDirname(import.meta.url);
+
+const files = getFiles(`${__dirname}/../kern`);
+
+const voices = [
+    {
+        voiceName: 'cantus',
+        spineIndex: 2,
+    },
+    {
+        voiceName: 'tenor',
+        spineIndex: 1,
+    },
+    {
+        voiceName: 'bassus',
+        spineIndex: 0,
+    },
+];
+
+files.forEach((file) => {
+    const id = getIdFromFilename(file);
+    const fb = execSync(`fb -i -a -m ${file} | extractxx -i **fb | ridx -LGTM`).toString().trim();
+
+    const piece = {
+        voices: {
+            bassus: {},
+            tenor: {},
+            cantus: {},
+        },
+    };
+
+    const lines = fb.split('\n');
+
+    voices.forEach(({voiceName, spineIndex}) => {
+        const fbNumbers = lines[lines.length - 2].split('\t');
+        const num = voiceName === 'bassus' ? '1' : fbNumbers[spineIndex];
+        piece.voices[voiceName].endSoundFiguredbassNumber = num;
+    });
+
+    const metaFile = `${__dirname}/../meta/${id}.yaml`;
+    const metaData = readYaml(metaFile);
+    mergeDeep(metaData, {voices: piece.voices});
+    writeYaml(metaFile, metaData);
+});
